e4466262321755b54e11f7faadcb85567e10d433
Diego Leonardo Urban <diego.urban88@gmail.com>
2014-08-05 19:20:51 -0300

    commit inicial projeta melanoma android

---
+++ Melanoma/src/br/furb/melanoma/MelanomaActivity.java
@@ -0,0 +1,199 @@
+package br.furb.melanoma;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Files.FileColumns;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+
+public class MelanomaActivity extends Activity {
+
+	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+	private static final int YOUR_SELECT_PICTURE_REQUEST_CODE = 0;
+	private Uri fileUri;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_melanoma);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.melanoma, menu);
+		return true;
+	}
+
+	private Uri outputFileUri;
+
+	private void openImageIntent() {
+
+		// Determine Uri of camera image to save.
+		final File root = new File(Environment.getExternalStorageDirectory()
+				+ File.separator + "MyDir" + File.separator);
+		root.mkdirs();
+		
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+				.format(new Date());
+			
+
+		final String fname = "IMG_demo_" + timeStamp + ".jpg";
+		final File sdImageMainDirectory = new File(root, fname);
+		outputFileUri = Uri.fromFile(sdImageMainDirectory);
+
+		// Camera.
+		final List<Intent> cameraIntents = new ArrayList<Intent>();
+		final Intent captureIntent = new Intent(
+				android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+		final PackageManager packageManager = getPackageManager();
+		final List<ResolveInfo> listCam = packageManager.queryIntentActivities(
+				captureIntent, 0);
+		for (ResolveInfo res : listCam) {
+			final String packageName = res.activityInfo.packageName;
+			final Intent intent = new Intent(captureIntent);
+			intent.setComponent(new ComponentName(res.activityInfo.packageName,
+					res.activityInfo.name));
+			intent.setPackage(packageName);
+			intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+			cameraIntents.add(intent);
+		}
+
+		// Filesystem.
+		final Intent galleryIntent = new Intent();
+		galleryIntent.setType("image/*");
+		galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+		// Chooser of filesystem options.
+		final Intent chooserIntent = Intent.createChooser(galleryIntent,
+				"Select Source");
+
+		// Add the camera options.
+		chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS,
+				cameraIntents.toArray(new Parcelable[] {}));
+
+		startActivityForResult(chooserIntent, YOUR_SELECT_PICTURE_REQUEST_CODE);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == RESULT_OK) {
+			if (requestCode == YOUR_SELECT_PICTURE_REQUEST_CODE) {
+				final boolean isCamera;
+				if (data == null) {
+					isCamera = true;
+				} else {
+					final String action = data.getAction();
+					if (action == null) {
+						isCamera = false;
+					} else {
+						isCamera = action
+								.equals(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+					}
+				}
+
+				Uri selectedImageUri;
+				if (isCamera) {
+					selectedImageUri = outputFileUri;
+				} else {
+					selectedImageUri = data == null ? null : data.getData();
+				}
+			}
+		}
+	}
+	
+	
+	
+	
+	
+
+	public void onCaptureImageButtonClicked(View view) {
+		// create Intent to take a picture and return control to the calling
+		// application
+		openImageIntent();
+		/*
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+		fileUri = getOutputMediaFileUri(FileColumns.MEDIA_TYPE_IMAGE); // create
+																		// a
+																		// file
+																		// to
+																		// save
+																		// the
+																		// image
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file
+															// name
+
+		// start the image capture Intent
+		startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+		*/
+	}
+
+	/** Create a file Uri for saving an image or video */
+	private static Uri getOutputMediaFileUri(int type) {
+		return Uri.fromFile(getOutputMediaFile(type));
+	}
+
+	/** Create a File for saving an image or video */
+	private static File getOutputMediaFile(int type) {
+		// To be safe, you should check that the SDCard is mounted
+		// using Environment.getExternalStorageState() before doing this.
+
+		if (!isExternalStorageWritable()) {
+			Log.d("external storage", "not available for read and write");
+		}
+
+		File mediaStorageDir = new File(
+				Environment
+						.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+				"MyCameraApp");
+
+		// This location works best if you want the created images to be shared
+		// between applications and persist after your app has been uninstalled.
+
+		// Create the storage directory if it does not exist
+		if (!mediaStorageDir.exists()) {
+			if (!mediaStorageDir.mkdirs()) {
+				Log.d("MyCameraApp", "failed to create directory");
+				return null;
+			}
+		}
+
+		// Create a media file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+				.format(new Date());
+		File mediaFile;
+		if (type == FileColumns.MEDIA_TYPE_IMAGE) {
+			mediaFile = new File(mediaStorageDir.getPath() + File.separator
+					+ "IMG_demo_" + timeStamp + ".jpg");
+		} else {
+			return null;
+		}
+
+		return mediaFile;
+	}
+
+	private static boolean isExternalStorageWritable() {
+		String state = Environment.getExternalStorageState();
+		if (Environment.MEDIA_MOUNTED.equals(state)) {
+			return true;
+		}
+		return false;
+	}
+
+}
